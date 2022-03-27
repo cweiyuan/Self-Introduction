327ee6b9c6dc5325e474dd3c986f656ef74f190e
cweiyuan <93870563+cweiyuan@users.noreply.github.com>
2022-03-27 19:58:44 +0800

    feat: initial commit
    This is my first commit.

---
+++ introduction.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main() 
+
+{
+	char characterName[10] = "Wei Yuan";
+	char str1[] = "wynaut";
+	int characterAge = 16;
+
+	printf("Hi, I am %s.\n", characterName);
+	printf("i am %d year old this year.\n", characterAge);
+	
+	characterAge = 17;
+
+  	printf("I will turn %d next year.\n", characterAge);
+  	printf("%s love playing piano amd programming\n", str1);
+  
+ 	printf("%s is day %d of programming \n","today",2);
+ 	printf("%f,\n",3.096686685437658);
+
+  	printf("   /|\n");
+  	printf("  / |\n");
+  	printf(" /  |\n");
+  	printf("/___|\n");
+ 
+	return 0;
+}
